17eb6847d7af60d0d738896a8823a3a7a3dc00b6
PhilippeVanVlierberghe <philippe.vv@outlook.com>
2019-11-07 10:52:03 +0100

    2019-11-7 test React site

---
+++ precache-manifest.3e5dc379fe08292693988078efd63c78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfa3d28d1c958ee07c3839e99ba011d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662d891535611008d4ed",
+    "url": "/static/css/main.72725477.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "/static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "662d891535611008d4ed",
+    "url": "/static/js/main.e5e49a54.chunk.js"
+  },
+  {
+    "revision": "8339aa2ba8bf82f99f91",
+    "url": "/static/js/runtime-main.27512ada.js"
+  }
+]);
