9f1e0348c993b9fd5c7fdb61b4d9a80c066ebdf8
PhilippeVanVlierberghe <philippe.vv@outlook.com>
2019-11-07 13:18:08 +0100

    2019-11-07 test fix React page load

---
+++ precache-manifest.17fdd7b145e0723543039bafecf06aab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87be68f3b8b766142b0588aecd0ca7b",
+    "url": "/React-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "662d891535611008d4ed",
+    "url": "/React-tic-tac-toe/static/css/main.72725477.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "/React-tic-tac-toe/static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "662d891535611008d4ed",
+    "url": "/React-tic-tac-toe/static/js/main.e5e49a54.chunk.js"
+  },
+  {
+    "revision": "6240499778d78cd7a218",
+    "url": "/React-tic-tac-toe/static/js/runtime-main.275a40c3.js"
+  }
+]);
